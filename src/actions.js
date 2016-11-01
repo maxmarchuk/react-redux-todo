b4208d16aff4e12f5b651da9cabe42e3c1812fc7
Max Marchuk <maxmarchuk@gmail.com>
2016-10-31 18:39:08 -0700

    Add redux actions file along with unique ID generator

---
+++ src/actions.js
@@ -0,0 +1,23 @@
+// redux actions
+
+//hack for generating unique ids
+let lastUID = 0;
+const getNextId = () => lastUID++;
+
+export function addTodo(text) {
+    return {
+        type: 'ADD_TODO',
+        payload: {
+            id: getNextId(),
+            isDone: false,
+            text
+        }
+    };
+}
+
+export function toggleToDo(id) {
+    return {
+        type: 'TOGGLE_TODO',
+        payload: id
+    }
+}
